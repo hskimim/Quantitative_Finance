46bc62c2bf38029c94b1c1b40e7e73bc1d1967ee
hskimim <hskimim8855@gmail.com>
2018-10-08 00:44:47 +0900

    function which for measuring the house price of korea

---
+++ SSU/personal_practice/House_Price_function.py
@@ -0,0 +1,78 @@
+def search_real_estate(loc='Seoul'):
+    '''
+    loc_option : Seoul , GyeongGi , Incheon , ChungcheongNam , ChungcheongBuk 
+    , JeollaNam , JeollaBuk , KyungsangNam , KyungsangBuk , Jeju , GangWon , DaeGu , Ulsan , Busan 
+    , SeJong , Daejeon , GwangJu
+    '''
+    if loc == 'Seoul': url = 'https://land.naver.com/article/cityInfo.nhn?cortarNo=1100000000&rletNo=&rletTypeCd=A01&tradeTypeCd=&hscpTypeCd=A01%3AA03%3AA04&cpId=&location=&siteOrderCode='
+    elif loc == 'GyeongGi':url = 'https://land.naver.com/article/cityInfo.nhn?cortarNo=4100000000&rletNo=&rletTypeCd=A01&tradeTypeCd=&hscpTypeCd=A01%3AA03%3AA04&cpId=&location=&siteOrderCode='
+    elif loc == 'Incheon':url = 'https://land.naver.com/article/cityInfo.nhn?cortarNo=2800000000&rletNo=&rletTypeCd=A01&tradeTypeCd=&hscpTypeCd=A01%3AA03%3AA04&cpId=&location=&siteOrderCode='
+    elif loc == 'ChungcheongNam':url = 'https://land.naver.com/article/cityInfo.nhn?cortarNo=4400000000&rletNo=&rletTypeCd=A01&tradeTypeCd=&hscpTypeCd=A01%3AA03%3AA04&cpId=&location=&siteOrderCode='
+    elif loc == 'ChungcheongBuk':url = 'https://land.naver.com/article/cityInfo.nhn?cortarNo=4300000000&rletNo=&rletTypeCd=A01&tradeTypeCd=&hscpTypeCd=A01%3AA03%3AA04&cpId=&location=&siteOrderCode='
+    elif loc == 'JeollaNam':url = 'https://land.naver.com/article/cityInfo.nhn?cortarNo=4600000000&rletNo=&rletTypeCd=A01&tradeTypeCd=&hscpTypeCd=A01%3AA03%3AA04&cpId=&location=&siteOrderCode='
+    elif loc == 'JeollaBuk':url = 'https://land.naver.com/article/cityInfo.nhn?cortarNo=4500000000&rletNo=&rletTypeCd=A01&tradeTypeCd=&hscpTypeCd=A01%3AA03%3AA04&cpId=&location=&siteOrderCode='
+    elif loc == 'KyungsangNam':url = 'https://land.naver.com/article/cityInfo.nhn?cortarNo=4800000000&rletNo=&rletTypeCd=A01&tradeTypeCd=&hscpTypeCd=A01%3AA03%3AA04&cpId=&location=&siteOrderCode='
+    elif loc == 'KyungsangBuk':url = 'https://land.naver.com/article/cityInfo.nhn?cortarNo=4700000000&rletNo=&rletTypeCd=A01&tradeTypeCd=&hscpTypeCd=A01%3AA03%3AA04&cpId=&location=&siteOrderCode='
+    elif loc == 'Jeju':url = 'https://land.naver.com/article/cityInfo.nhn?cortarNo=5000000000&rletNo=&rletTypeCd=A01&tradeTypeCd=&hscpTypeCd=A01%3AA03%3AA04&cpId=&location=&siteOrderCode='
+    elif loc == 'GangWon':url = 'https://land.naver.com/article/cityInfo.nhn?cortarNo=4200000000&rletNo=&rletTypeCd=A01&tradeTypeCd=&hscpTypeCd=A01%3AA03%3AA04&cpId=&location=&siteOrderCode='
+    elif loc == 'DaeGu':url = 'https://land.naver.com/article/cityInfo.nhn?cortarNo=2700000000&rletNo=&rletTypeCd=A01&tradeTypeCd=&hscpTypeCd=A01%3AA03%3AA04&cpId=&location=&siteOrderCode='
+    elif loc == 'Ulsan':url = 'https://land.naver.com/article/cityInfo.nhn?cortarNo=3100000000&rletNo=&rletTypeCd=A01&tradeTypeCd=&hscpTypeCd=A01%3AA03%3AA04&cpId=&location=&siteOrderCode='
+    elif loc == 'Busan':url = 'https://land.naver.com/article/cityInfo.nhn?cortarNo=2600000000&rletNo=&rletTypeCd=A01&tradeTypeCd=&hscpTypeCd=A01%3AA03%3AA04&cpId=&location=&siteOrderCode='
+    elif loc == 'SeJong':url = 'https://land.naver.com/article/divisionArticleList.nhn?rletTypeCd=A01&tradeTypeCd=&cortarNo=3611000000'
+    elif loc == 'Daejeon':url = 'https://land.naver.com/article/cityInfo.nhn?rletTypeCd=A01&tradeTypeCd=&hscpTypeCd=&cortarNo=3000000000'
+    else : url = 'https://land.naver.com/article/cityInfo.nhn?cortarNo=2900000000&rletNo=&rletTypeCd=A01&tradeTypeCd=&hscpTypeCd=&cpId=&location=&siteOrderCode='        
+        
+    location_name = []
+    house_name = []
+    house_price = []
+
+    driver = webdriver.Chrome()
+    driver.get(url)
+    time.sleep(2)
+
+    for i in range(1,51):
+        try : 
+            if len(location_name) > 1 : location_name.pop()
+            location_name.append(driver.find_element_by_css_selector('#regionListArea > div.area.scroll > ul > li:nth-child({}) > a'.format(i)).text)
+            driver.find_element_by_css_selector('#regionListArea > div.area.scroll > ul > li:nth-child({}) > a'.format(i)).click()
+        except : break
+
+        for _ in range(50):
+            for j in range(1,50+1):
+                driver.execute_script('window.scrollTo(100,{});'.format((j+4)*100))
+                try : 
+                    #time.sleep(1)
+                    house_name.append(driver.find_element_by_css_selector('#depth4Tab0Content > div > table > tbody > tr:nth-child({}) > td.align_l.name > div > a'.format(2*j-1)).text)
+                    house_price.append(driver.find_element_by_css_selector('#depth4Tab0Content > div > table > tbody > tr:nth-child({}) > td.num.align_r > div > strong'.format(2*j-1)).text)
+                    location_name.append(location_name[-1])
+                except : pass
+
+            try : 
+                time.sleep(1)
+                driver.find_element_by_xpath('//*[@id="depth4Tab0Content"]/div/div/div/a[{}]'.format(11)).click()
+                driver.execute_script('window.scrollTo(100,{});'.format(0))
+            except : 
+                driver.find_element_by_xpath('//*[@id="depth4Tab0Content"]/div/div/div/a[{}]'.format(10)).click()
+                time.sleep(1)
+                driver.execute_script('window.scrollTo(100,{});'.format(0))
+
+        driver.get(url)
+    driver.close()
+    
+    return location_name , house_name , house_price
+
+
+def tuning_df(df):
+    new_house_price = []
+    for i in (df['house_price']):
+        if '/' in i : 
+            new_house_price.append(i[:i.find('/')])
+        else : 
+            new_house_price.append(i)
+    display(len(new_house_price))
+    
+    df['house_price'] = new_house_price
+    df['house_price'] = df['house_price'].apply(lambda x : x.replace(',',''))
+    df['house_price'] = [int(i) for i in df['house_price']]
+    
+    return df
