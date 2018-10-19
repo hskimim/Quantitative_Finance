15068eb43a0deefaa0fb066e804cb93fd3542910
hskimim <hskimim8855@gmail.com>
2018-10-19 09:53:04 +0900

    newly updated function

--- SSU/finlab/extract_reference/extract_reference_type1.py
+++ SSU/finlab/extract_reference/extract_reference_type1.py
@@ -1,6 +1,8 @@
 import io
 import re
 import pandas as pd
+import numpy as np
+from IPython.display import display , Markdown
 
 from pdfminer.converter import TextConverter
 from pdfminer.pdfinterp import PDFPageInterpreter
@@ -32,6 +34,38 @@ def extract_text_from_pdf(pdf_path):
     if text:
         return text
 
+def find_page_idx(file_):
+    page_ls = []
+    page_idx = [re.search((re.findall('\x0c[\d]+',file_)[i]),file_).end() for i in range(len(re.findall('\x0c[\d]+',file_)))]
+    for idx,val in enumerate(page_idx):
+        if idx ==0 :
+            page_ls.append(file_[:val])
+        else : page_ls.append(file_[page_idx[idx-1]:page_idx[idx]])
+    return page_ls
+
+def find_start_page(file_):
+    ls = ['references','References','REFERENCES','참 고 문 헌' , '참고문헌','<참 고 문 헌>']
+    ref_page = [idx for idx,val in enumerate(find_page_idx(file_)) for i in ls if i in val][0]
+    return ref_page
+
+def find_end_page(file_):
+    ls2 = ['Appendix','appendix','APPENDIX','부록','부 록','표 1','테이블 1','테 이 블 1','table 1','Table 1','TABLE 1','Endnotes']
+    end_page = [idx for idx,i in enumerate(find_page_idx(file_)[find_start_page(file_):]) for j in ls2 if j in i]
+    if end_page != [] : return end_page[-1]
+    else :
+        display(Markdown('### This function gonna call the find_end_page_exception'))
+        return find_end_page_ex(file_)
+
+def find_end_page_ex(file_):
+    page_file = find_page_idx(file_)
+    start_page = find_start_page(file_)
+    ls3 = ['“' , '”' , '19[0-9]{2}' , '20[0-9]{2}' , 'Journal' , '[0-9]+[-][0-9]+']
+    if len(page_file[start_page:]) == 1 : return 1
+    for idx,page in enumerate(reversed(page_file[start_page:])):
+        for i in ls3 :
+            if len(re.findall(i,page)) <2 : pass
+            else : return  len(page_file[start_page:])-idx-1
+
 def find_start_idx(file_) :
     ls = ['references','References','REFERENCES','참 고 문 헌' , '참고문헌','<참 고 문 헌>']
     start_idx = []
@@ -39,8 +73,30 @@ def find_start_idx(file_) :
     return start_idx
 
 def find_end_idx(file_):
-    end_idx = re.search(re.findall('[0-9]+[-][0-9]+',file_)[-1],file_).end()
+    ls1 = ['표 1','테이블 1','테 이 블 1','table 1','Table 1','TABLE 1']
+    ls2 = ['Appendix','appendix','APPENDIX','부록','부 록','Endnotes']
+    try :
+        try :
+            testing_ls = [re.search(i,file_) for i in ls1]
+            testing_ls = [idx for idx,val in enumerate(testing_ls) if val != None][0]
+            end_idx =[(m.start(0), m.end(0)) for m in re.finditer(ls[testing_ls],file_)][-1][0]
+            if end_idx > find_start_idx(file_):
+                return end_idx
+        except :
+            testing_ls = [re.search(i,file_) for i in ls2]
+            testing_ls = [idx for idx,val in enumerate(testing_ls) if val != None][0]
+            end_idx =[(m.start(0), m.end(0)) for m in re.finditer(ls[testing_ls],file_)][-1][0]
+            if end_idx > find_start_idx(file_):
+                return end_idx
+    except :
+        end_idx = re.search(re.findall('[0-9]+[-][0-9]+',file_)[-1],file_).end()
     return end_idx
+
+def make_references_page(file_):
+    page_file = find_page_idx(file_)
+    start_page = find_start_page(file_)
+    end_page = find_end_page(file_)
+    return page_file[start_page:start_page+end_page]
 
 def make_references(file_):
     start_idx = find_start_idx(file_)
